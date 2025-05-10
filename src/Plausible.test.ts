bf0b0b48b8500bd7da461aaa8836e894a5ec97cd
Robert Vitonsky <rob@vitonsky.net>
2025-05-10 17:02:36 +0200

    test: add trivial test

---
+++ src/Plausible.test.ts
@@ -0,0 +1,47 @@
+import { Plausible } from './Plausible';
+
+const mockFetch = vi.fn();
+globalThis.fetch = mockFetch;
+
+beforeEach(() => {
+	vi.clearAllMocks();
+
+	Object.defineProperty(window, 'location', {
+		value: {
+			href: 'https://example.org/',
+			assign: vi.fn(),
+			replace: vi.fn(),
+		},
+		writable: true,
+	});
+});
+
+test('should call fetch with correct params', async () => {
+	mockFetch.mockReturnValue(new Response('ok'));
+
+	const plausible = new Plausible({
+		apiHost: 'https://plausible.io',
+		domain: 'example.org',
+	});
+
+	expect(plausible.sendEvent('test')).resolves.toBeUndefined();
+
+	expect(mockFetch.mock.calls).toEqual([
+		[
+			'https://plausible.io/api/event',
+			{
+				method: 'POST',
+				headers: { 'Content-Type': 'text/plain' },
+				body: JSON.stringify({
+					n: 'test',
+					u: 'https://example.org/',
+					d: 'example.org',
+					r: null,
+					w: 1024,
+					h: 0,
+				}),
+				keepalive: true,
+			},
+		],
+	]);
+});
